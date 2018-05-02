1881fd8b467285975cafe92d2f3c154bce74c7cc
havidzc0de <havidzc0de@gmail.com>
2018-05-02 12:42:53 +0700

    [havidzc0de] - Add Class Polindrome

---
+++ praktek/OOP/Class.rb
@@ -0,0 +1,33 @@
+#!/bin/ruby
+
+hewan = ["kodok","katak","sapi"]
+
+
+class Polindrome
+
+	def initialize(hewan)
+		@hewan = hewan
+	end
+
+	def cek()
+		(0..@hewan.length-1).each do |i|
+			if @hewan[i] === @hewan[i].reverse then
+				puts "#{@hewan[i]} adalah Polindrome"
+			elsif @hewan[i] === @hewan[i]
+				puts "#{@hewan[i]} Bukan Polindrome"
+			end
+		end	
+	end
+end
+
+
+objek_polindrome = Polindrome.new(hewan)
+objek_polindrome.cek()
+
+
+
+
+
+
+
+
