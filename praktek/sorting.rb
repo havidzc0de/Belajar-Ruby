ffc7f73cd06c43f4404fc1ecdd1f243f6f686ed3
zetc0de <havidzc0de@gmail.com>
2018-05-10 01:01:26 +0700

    Algoritma Sorting

---
+++ praktek/sorting.rb
@@ -0,0 +1,21 @@
+ 	#!/bin/ruby
+
+array = [5,2,6,9,4,5,7]
+
+
+def anu(array)
+	n = array.length
+	loop do
+		swapped = false
+		(n-1).times do |i|
+			if array[i] > array[i+1]
+				array[i],array[i+1] = array[i+1],array[i]
+				swapped = true
+			end
+		end
+		break if swapped == false
+	end
+	return array
+end
+
+puts anu(array)
